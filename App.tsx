f87a06bb18992b82bd5e638f4d36dd4df3fbca46
Carl Justine Olavario <carljustineolavario18@gmail.com>
2025-12-29 06:09:02 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -64,7 +64,7 @@ const App: React.FC = () => {
           <p className="font-symphony text-4xl sm:text-5xl md:text-7xl text-red-800 mb-6">
             Vince <span className="font-script">&</span> Casian
           </p>
-          <p className="text-gray-400 uppercase tracking-[0.3em] text-[9px] md:text-xs">Forever & Always • July 11, 2026</p>
+          <p className="text-gray-400 uppercase tracking-[0.3em] text-[9px] md:text-xs">Forever & Always • May Date, 2026</p>
           <div className="mt-8 flex justify-center items-center space-x-4 md:space-x-6">
              <div className="w-8 md:w-20 h-px bg-red-100"></div>
              <span className="text-red-600 text-lg md:text-xl">❤</span>
